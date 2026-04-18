2306853eef85fd2b2521e5b6ba795c44b6a53590
Nour1717 <elabed.nour@gmail.com>
2026-04-18 13:26:58 +0100

    update app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,21 @@ import Upload from "./components/Upload";
 function App() {
   return (
     <>
-      <style>
-        {`
-          @keyframes spin {
-            to {
-              transform: rotate(360deg);
-            }
+      <style>{`
+        @keyframes spin {
+          to {
+            transform: rotate(360deg);
           }
-        `}
-      </style>
-      
-
+        }
+        * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+        }
+        body {
+          background: #f0f4ff;
+        }
+      `}</style>
       <Upload />
     </>
   );
